a1c8cc812d777d45866ceac1ae626e1e1f36c131
HooDeLin <hoodelin0619@gmail.com>
2017-10-01 14:06:37 +0800

    Updates

--- delin/integrate.sh
+++ delin/integrate.sh
@@ -3,5 +3,5 @@ while true; do
   imagesnap -w 1 input.jpg 1>/dev/null
   sips -Z 512 input.jpg 1>/dev/null 2>/dev/null
   ./darknet detector test cfg/voc.data cfg/tiny-yolo-voc.cfg tiny-yolo-voc.weights input.jpg 1>/dev/null 2>/dev/null &
-  sleep 0.2
+  sleep 0.1
 done
